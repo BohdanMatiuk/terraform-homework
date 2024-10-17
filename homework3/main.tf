627cac7a87bf4d54d40727a5c0323546e23fdbd5
BohdanMatiuk <mbs.dvp@gmail.com>
2024-10-17 21:36:20 +0000

    3-4

---
+++ homework3/main.tf
@@ -0,0 +1,62 @@
+provider aws {
+    region = "us-east-2"
+}
+
+resource "aws_key_pair" "homework" {
+  key_name   = "homework"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "instance1" {
+  ami = data.aws_ami.linux2.id
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.homework.key_name
+  availability_zone = "us-east-2a"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache2.sh")
+  
+  tags = {
+  Name = "web1"
+  }
+}
+resource "aws_instance" "instance2" {
+  ami = data.aws_ami.linux2.id
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.homework.key_name
+  availability_zone = "us-east-2b"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache2.sh")
+  
+  tags = {
+  Name = "web2"
+  }
+}
+
+resource "aws_instance" "instance3" {
+  ami = data.aws_ami.linux2.id
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.homework.key_name
+  availability_zone = "us-east-2c"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache2.sh")
+  
+  tags = {
+  Name = "web3"
+  }
+}
+  
+  data "aws_ami" "linux2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"]
+}
